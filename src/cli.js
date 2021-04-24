bdf517c0f4723b5aae7b206f9fc95cbb50851209
Vyacheslav <kokorinv19@gmail.com>
2021-04-24 23:24:24 +0500

    modified: src/cli.js src/game-even.js

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,10 @@
 import readlineSync from 'readline-sync';
 
-export let userName = '';
-
+export const user = { name: '' };
 const userGreeting = () => {
   console.log('Welcome to the Brain Games!');
-  userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+  user.name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${user.name}!`);
 };
 
 export default userGreeting;
--- src/game-even.js
+++ src/game-even.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
-import { userName } from './cli.js';
+import { user } from './cli.js';
 
 const getRandomNumber = () => {
   const minRandomNum = 1;
@@ -9,7 +9,8 @@ const getRandomNumber = () => {
 };
 
 const isCorrectAnswer = (randomNum, userAnswer) => {
-  return (randomNum % 2 === 0 && userAnswer === 'yes') || (randomNum % 2 !== 0 && userAnswer === 'no');
+  const сorrectAnswer = (randomNum % 2 === 0 && userAnswer === 'yes') || (randomNum % 2 !== 0 && userAnswer === 'no');
+  return сorrectAnswer;
 };
 
 const correctAnswer = (num) => (num % 2 === 0 ? 'yes' : 'no');
@@ -19,15 +20,15 @@ const correctAnswersToWin = 3;
 const even = () => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 1; i <= correctAnswersToWin; i += 1) {
-    let randomNum = getRandomNumber();
+    const randomNum = getRandomNumber();
     console.log(`Question: ${randomNum}`);
-    let userAnswer = readlineSync.question('Your answer: ');
+    const userAnswer = readlineSync.question('Your answer: ');
     if (!isCorrectAnswer(randomNum, userAnswer)) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer(randomNum)}.`);
-      console.log(`Let's try again, ${userName}!`);
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer(randomNum)}'.`);
+      console.log(`Let's try again, ${user.name}!`);
       break;
     } else if (isCorrectAnswer(randomNum, userAnswer) && i === correctAnswersToWin) {
-      console.log(`Congratulations, ${userName}!`);
+      console.log(`Congratulations, ${user.name}!`);
       break;
     } else {
       console.log('Correct!');
