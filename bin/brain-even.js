ae34f79e3bf310424a58013ac752eabf825d9723
Vyacheslav <kokorinv19@gmail.com>
2021-04-25 19:02:57 +0500

    modified bin/brain-even.js bin/brain-games.js package.json

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,37 @@
 #!/usr/bin/env node
-import userGreeting from '../src/cli.js';
-import even from '../src/game-even.js';
+import readlineSync from 'readline-sync';
+import userGreeting, { user } from '../src/cli.js';
+import { maxNumOfGameRounds, getRandomNumber } from '../src/index.js';
 
 userGreeting();
-even();
+
+const gameTask = 'Answer "yes" if the number is even, otherwise answer "no".';
+console.log(gameTask);
+
+const getCorrectAnswer = (num) => (num % 2 === 0 ? 'yes' : 'no');
+
+const isCorrectUserAnswer = (randomNum, userAnswer) => {
+  const сorrectAnswer = getCorrectAnswer(randomNum) === userAnswer;
+  return сorrectAnswer;
+};
+
+const getQuestion = () => {
+  const question = `${getRandomNumber()}`;
+  return question;
+};
+
+for (let i = 1; i <= maxNumOfGameRounds; i += 1) {
+  const question = getQuestion();
+  console.log(`Question: ${question}`);
+  const userAnswer = readlineSync.question('Your answer: ');
+  if (!isCorrectUserAnswer(question, userAnswer)) {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${getCorrectAnswer(question)}'.`);
+    console.log(`Let's try again, ${user.name}!`);
+    break;
+  } else if (isCorrectUserAnswer(question, userAnswer) && i === maxNumOfGameRounds) {
+    console.log(`Congratulations, ${user.name}!`);
+    break;
+  } else {
+    console.log('Correct!');
+  }
+}
