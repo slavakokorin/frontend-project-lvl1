292a4a9018c024d693b339f99450e12d3882c3b9
Vyacheslav <kokorinv19@gmail.com>
2021-05-09 15:36:27 +0500

    modified src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,14 +1,9 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ');
 const userGreeting = () => {
-  const saveName = userName;
-  const printName = () => saveName;
-  return printName;
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
-console.log(`Hello, ${userName}!`);
-
-export const getUserName = userGreeting();
 
 export default userGreeting;
