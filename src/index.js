071073dfa3ba3ce678809e250ac24ed125074054
Vyacheslav <kokorinv19@gmail.com>
2021-05-20 10:35:35 +0500

    modified src/index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import readlineSync from 'readline-sync';
 
-export default (getGameRound, gameTask) => {
+const roundsCount = 3;
+
+export default (getRound, gameTask) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(gameTask);
-  if (getGameRound()) {
-    return console.log(`Congratulations, ${userName}!`);
+  for (let i = 0; i < roundsCount; i += 1) {
+    const [gameQuestion, answer] = getRound();
+    console.log(`Question: ${gameQuestion}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (answer !== userAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`);
+      return console.log(`Let's try again, ${userName}!`);
+    }
+    console.log('Correct!');
   }
-  return console.log(`Let's try again, ${userName}!`);
+  return console.log(`Congratulations, ${userName}!`);
 };
