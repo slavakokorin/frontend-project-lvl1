1b18aef4c6e04da271b6931636a6dd6c7f672c58
Vyacheslav <kokorinv19@gmail.com>
2021-06-08 22:34:47 +0500

    modifird even.js

--- src/games/even.js
+++ src/games/even.js
@@ -3,20 +3,12 @@ import startGame from '../index.js';
 
 const gameTask = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isNumberEven = (num) => num % 2 === 0;
-
-export const getQuestionInEvenGame = () => {
-  const question = `${getRandomNumber(1, 20)}`;
-  return question;
-};
+const isEven = (num) => num % 2 === 0;
 
 const getRound = () => {
-  const gameQuestion = getQuestionInEvenGame();
-  let answer = 'no';
-  if (isNumberEven(gameQuestion)) {
-    answer = 'yes';
-  }
-  return [gameQuestion, answer];
+  const question = `${getRandomNumber(1, 20)}`;
+  const answer = isEven(question) ? 'yes' : 'no';
+  return [question, answer];
 };
 
 export default () => startGame(getRound, gameTask);
