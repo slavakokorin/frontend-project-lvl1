2a71adb86f8a5b24e484edd2af38d8c43801d6a5
Vyacheslav <kokorinv19@gmail.com>
2021-06-10 01:21:45 +0500

    modified src/games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,29 +3,30 @@ import startGame from '../index.js';
 
 const gameTask = 'What number is missing in the progression?';
 
-const getArithmeticProgression = () => {
-  const progressionLength = getRandomNumber(5, 10);
-  const progressionStep = getRandomNumber(2, 5);
+const getProgression = (first, step, length) => {
   const progression = [];
-  const firstNum = getRandomNumber(1, 20);
-  progression.push(firstNum);
-  let nextProgressNum = firstNum;
-  for (let i = 1; i < progressionLength; i += 1) {
-    nextProgressNum += progressionStep;
+  progression.push(first);
+  let nextProgressNum = first;
+  for (let i = 1; i < length; i += 1) {
+    nextProgressNum += step;
     progression.push(nextProgressNum);
   }
   return progression;
 };
 
 export const getRound = () => {
-  const progression = getArithmeticProgression();
-  const secretNum = getRandomNumber(0, progression.length - 1);
-  const answer = progression[secretNum].toString();
-  progression[secretNum] = '..';
-  let question = '';
-  for (let i = 0; i < progression.length; i += 1) {
-    question += `${progression[i]} `;
-  }
+  const progressionLength = getRandomNumber(5, 10);
+  const progressionStep = getRandomNumber(2, 5);
+  const first = getRandomNumber(1, 20);
+  const progression = getProgression(first, progressionStep, progressionLength);
+  const secretIndex = getRandomNumber(0, progression.length - 1);
+  const answer = progression[secretIndex].toString();
+  progression[secretIndex] = '..';
+  // let question = '';
+  // for (let i = 0; i < progression.length; i += 1) {
+  //   question += `${progression[i]} `;
+  // }
+  const question = progression.join(' ');
   return [question, answer];
 };
 
