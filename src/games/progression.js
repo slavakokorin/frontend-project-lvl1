19d23f752deccc688d6f22f56fccbfce42f493ea
Vyacheslav <kokorinv19@gmail.com>
2021-06-11 00:10:25 +0500

    modified src/games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,27 +5,22 @@ const gameTask = 'What number is missing in the progression?';
 
 const getProgression = (first, step, length) => {
   const progression = [];
-  progression.push(first);
   let nextProgressNum = first;
-  for (let i = 1; i < length; i += 1) {
+  for (let i = 0; i < length; i += 1) {
     nextProgressNum += step;
     progression.push(nextProgressNum);
   }
   return progression;
 };
 
-export const getRound = () => {
-  const progressionLength = getRandomNumber(5, 10);
-  const progressionStep = getRandomNumber(2, 5);
+const getRound = () => {
+  const length = getRandomNumber(5, 10);
+  const step = getRandomNumber(2, 5);
   const first = getRandomNumber(1, 20);
-  const progression = getProgression(first, progressionStep, progressionLength);
+  const progression = getProgression(first, step, length);
   const secretIndex = getRandomNumber(0, progression.length - 1);
   const answer = progression[secretIndex].toString();
   progression[secretIndex] = '..';
-  // let question = '';
-  // for (let i = 0; i < progression.length; i += 1) {
-  //   question += `${progression[i]} `;
-  // }
   const question = progression.join(' ');
   return [question, answer];
 };
